bc2d7d9ae50ee0b9f1e63fae55819a61f1400c0c
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-08-24 10:27:50 -0700

    use context and use reducer

--- src/components/NewestPerson.js
+++ src/components/NewestPerson.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import PeopleCount from './PeopleCount';
+import PeopleContext from '../context/peopleContext';
 
-const NewestPerson = ({ newestPerson }) => {
+const NewestPerson = () => {
+
+  const context = useContext(PeopleContext);
+  const newestPerson = context.people[context.people.length - 1];
+
   useEffect(() => {
+
     const newestPersonName = `${newestPerson.firstName} ${
       newestPerson.lastName}`;
     document.title = newestPersonName;
@@ -10,11 +17,13 @@ const NewestPerson = ({ newestPerson }) => {
       console.log('Unmounted');
     };
   }, [newestPerson]);
+
   return (
     <div className="col col-sm-12">
       <h2 className="mt-4 text-center">
         Newest Person: {`${newestPerson.firstName} ${newestPerson.lastName}`}
       </h2>
+      <PeopleCount />
     </div>
   );
 };
